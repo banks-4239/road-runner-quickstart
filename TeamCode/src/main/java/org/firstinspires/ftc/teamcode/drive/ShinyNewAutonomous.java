2511206c39db31aae3d2a84d69788ef07a6b3c7e
Bledsoe <61562506+karlbledsoe@users.noreply.github.com>
2022-03-05 15:26:42 -0800

    g

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/ShinyNewAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/ShinyNewAutonomous.java
@@ -55,3 +55,319 @@ public class ShinyNewAutonomous extends LinearOpMode {
     Pose2d redIntermediate3 = new Pose2d(38, -65.4, Math.toRadians(0));
     Pose2d redIntermediate4 = new Pose2d(38, -38, Math.toRadians(0));
 
+    public int hubNum;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        rb.init(hardwareMap);
+
+        initVuforia();
+        initTfod();
+
+
+        telemetry.addData("Please choose a mode!", "up - redDuck, right - redWarehouse, left - blueDuck, down - blueWarehouse");
+        telemetry.update();
+
+        while (!rb.choosingAuto) {
+
+
+            telemetry.addData("Please choose a setting", "");
+            if (rb.redOrBlue) {
+                telemetry.addData("Red", rb.settings[rb.onSetting - 1]);
+                telemetry.addData("Red", ">" + rb.settings[rb.onSetting]);
+                telemetry.addData("Red", rb.settings[rb.onSetting + 1]);
+            } else {
+                telemetry.addData("Blue", rb.settings[rb.onSetting - 1]);
+                telemetry.addData("Blue", ">" + rb.settings[rb.onSetting]);
+                telemetry.addData("Blue", rb.settings[rb.onSetting + 1]);
+            }
+
+            telemetry.addData("hub", hubNum);
+
+            if (gamepad1.dpad_up) {
+                if (rb.onSetting > 1 && !rb.settingButtonDown1) {
+                    rb.onSetting--;
+                }
+                rb.settingButtonDown1 = true;
+            } else {
+                rb.settingButtonDown1 = false;
+            }
+
+            if (gamepad1.dpad_down) {
+                if (rb.onSetting < 4 && !rb.settingButtonDown2) {
+                    rb.onSetting++;
+                }
+                rb.settingButtonDown2 = true;
+            } else {
+                rb.settingButtonDown2 = false;
+            }
+
+            if (gamepad1.dpad_left) {
+                rb.redOrBlue = true;
+            } else if (gamepad1.dpad_right) {
+                rb.redOrBlue = false;
+            }
+
+            if (isStarted()) {
+                if (rb.redOrBlue) {
+                    rb.choosingAuto = true;
+                    rb.autoMode = rb.onSetting;
+                } else {
+                    rb.choosingAuto = true;
+                    rb.autoMode = rb.onSetting + 4;
+                }
+            }
+            hubNum = getElement();
+            telemetry.update();
+
+
+        }
+
+        waitForStart();
+
+
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+
+        switch (rb.autoMode) {
+            case 1:
+                redDuckWithFreight();
+                break;
+            case 2:
+                redDuckWithNoFreight();
+                break;
+            case 3:
+                redWarehouseWithFreight();
+                break;
+            case 4:
+                redWarehouseWithNoFreight();
+                break;
+            case 5:
+                blueDuckWithFreight();
+                //flipHub();
+                break;
+            case 6:
+                blueDuckWithNoFreight();
+                //flipHub();
+                break;
+            case 7:
+                blueWarehouseWithFreight();
+                //flipHub();
+                break;
+            case 8:
+                blueWarehouseWithNoFreight();
+                //flipHub();
+                break;
+
+        }
+
+
+
+    }
+
+
+    public void spinnerRed(double speed) {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        drive.spinnerR.setPower(-speed);
+        drive.spinnerL.setPower(-speed);
+    }
+
+    public void spinnerBlue(double speed) {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        drive.spinnerR.setPower(speed);
+        drive.spinnerL.setPower(speed);
+    }
+
+    public void spinnerEnd() {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        drive.spinnerR.setPower(0);
+        drive.spinnerL.setPower(0);
+    }
+
+    public void redDuckWithFreight(){
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Trajectory red1 = drive.trajectoryBuilder(new Pose2d(-41, -63, Math.toRadians(-90)))
+                .lineToLinearHeading(duckSpinRed)
+                .build();
+
+        Trajectory red2 = drive.trajectoryBuilder(red1.end())
+                .lineToLinearHeading(new Pose2d(new Vector2d(-33, -24), Math.toRadians(0)))
+                .build();
+
+        Trajectory red3 = drive.trajectoryBuilder(red2.end())
+                .lineToLinearHeading(new Pose2d(new Vector2d(-63, -37), Math.toRadians(180)))
+                .build();
+
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+        drive.setPoseEstimate(new Pose2d(-41, -63, Math.toRadians(-90)));
+        drive.followTrajectory(red1);
+        drive.followTrajectory(red2);
+        drive.followTrajectory(red3);
+    }
+    public void redDuckWithNoFreight(){
+
+    }
+    public void redWarehouseWithFreight(){
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Trajectory red1 = drive.trajectoryBuilder(startPosRedWarehouse)
+                .lineToLinearHeading(freightRedWarehouse)
+                .build();
+
+        Trajectory red2 = drive.trajectoryBuilder(red1.end())
+                .lineToLinearHeading(redIntermediate2)
+                .build();
+
+        Trajectory red3 = drive.trajectoryBuilder(red1.end())
+                .lineToLinearHeading(redIntermediate3)
+                .build();
+
+        Trajectory red4 = drive.trajectoryBuilder(red1.end())
+                .lineToLinearHeading(redIntermediate4)
+                .build();
+
+        Trajectory red5 = drive.trajectoryBuilder(red1.end())
+                .lineToLinearHeading(endPosRedWarehouse)
+                .build();
+
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+        drive.setPoseEstimate(startPosRedWarehouse);
+        drive.followTrajectory(red1);
+        drive.followTrajectory(red2);
+        drive.followTrajectory(red3);
+        drive.followTrajectory(red4);
+        drive.followTrajectory(red5);
+    }
+    public void redWarehouseWithNoFreight(){
+
+    }
+    public void blueDuckWithFreight(){
+
+    }
+    public void blueDuckWithNoFreight(){
+
+    }
+    public void blueWarehouseWithFreight(){
+
+    }
+    public void blueWarehouseWithNoFreight(){
+
+    }
+
+
+
+
+
+    public int getElement(){
+
+        if (rb.tfod != null) {
+            // getUpdatedRecognitions() will return null if no new information is available since
+            // the last time that call was made.
+            List<Recognition> updatedRecognitions = rb.tfod.getUpdatedRecognitions();
+            if (updatedRecognitions != null) {
+                // telemetry.addData("# Object Detected", updatedRecognitions.size());
+                // step through the list of recognitions and display boundary info.
+                int i = 0;
+                for (i = 0; i != updatedRecognitions.size(); i++) {
+                        /*telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                        telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                                recognition.getLeft(), recognition.getTop());
+                        telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                                recognition.getRight(), recognition.getBottom());*/
+
+                    if(updatedRecognitions.get(i).getLabel() != "Marker") {
+                        telemetry.addData("", updatedRecognitions.get(0).getLeft());
+                        if(updatedRecognitions.get(0).getLeft() < 180){
+                            return 1;
+                        }else{
+                            return 2;
+                        }
+                    }
+
+
+
+
+
+
+                }
+                if(updatedRecognitions.size() == 0){
+                    return 3;
+                }
+
+
+
+
+
+            }
+
+        }
+        return hubNum;
+    }
+
+    private void initVuforia() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = rb.VUFORIA_KEY;
+        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+        //  Instantiate the Vuforia engine
+        rb.vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        // Loading trackables is not necessary for the TensorFlow Object Detection engine.
+    }
+
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minResultConfidence = 0.8f;
+        tfodParameters.isModelTensorFlow2 = true;
+        tfodParameters.inputSize = 320;
+        rb.tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, rb.vuforia);
+        rb.tfod.loadModelFromAsset(rb.TFOD_MODEL_ASSET, rb.LABELS);
+    }
+
+
+
+
+
+
+}
+/*public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        Trajectory[] trajarray = new Trajectory[2];
+
+
+
+        trajarray[0] = drive.trajectoryBuilder(new Pose2d())
+                .strafeRight(10)
+                .build();
+
+        trajarray[1]  = drive.trajectoryBuilder(trajarray[0].end())
+                .forward(5)
+                .build();
+
+        waitForStart();
+
+        if(isStopRequested()) return;
+
+        for(int i = 0; i == trajarray.length; i++){
+            drive.followTrajectory(trajarray[i]);
+        }
+
+    }*/
