dd2d15b1efb284efd9f6c725d94935b3f10d1f30
Bledsoe <61562506+karlbledsoe@users.noreply.github.com>
2022-03-05 15:48:57 -0800

    Update ShinyNewAutonomous.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/ShinyNewAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/ShinyNewAutonomous.java
@@ -190,7 +190,7 @@ public class ShinyNewAutonomous extends LinearOpMode {
         drive.spinnerL.setPower(0);
     }
 
-    public void redToStorageUnit(){
+    public void redDuckToStorageUnit(){
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
         Trajectory red1 = drive.trajectoryBuilder(new Pose2d(-41, -63, Math.toRadians(-90)))
                 .lineToLinearHeading(duckSpinRed)
@@ -218,6 +218,9 @@ public class ShinyNewAutonomous extends LinearOpMode {
     }
     public void redWarehouseWithFreight(){
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+
+
         Trajectory red1 = drive.trajectoryBuilder(startPosRedWarehouse)
                 .lineToLinearHeading(freightRedWarehouse)
                 .build();
@@ -265,7 +268,8 @@ public class ShinyNewAutonomous extends LinearOpMode {
 
     }
 
-
+    //public Trajectory switchTraj(){
+    //}
 
 
 
